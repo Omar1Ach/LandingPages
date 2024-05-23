47c2084fb6e1bd37550396a915b4604b20151670
omar <omar.achbani@outlook.com>
2024-05-23 11:51:13 +0100

    start

---
+++ MicrosoftLP/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div>
+      <div>
+        <h2>Microsoft</h2>
+        <ul>
+            <li>Home</li>
+            <li>Products</li>
+            <li>Services</li>
+            <li>About</li>
+            <li>Contact</li>
+            <li>Sign In</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
